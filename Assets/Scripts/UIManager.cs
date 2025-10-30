c31afac46ad3ef174ca3e00581d9001d860e8dfa
JacobBaskiewicz1 <jacobbaskiewicz@gmail.com>
2025-10-30 18:04:36 -0500

    fix to crash and audio, pause menu added.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject pauseMenu;
+    public FirstPersonController player;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        player.cameraCanMove = false;
+    }
+
+    public void Return()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        player.cameraCanMove = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
